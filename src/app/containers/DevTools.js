112a54b0bbc762a01db0b53ffd5616aab954cb4c
Jax <jaxchen.oz@gmail.com>
2016-01-18 16:06:34 +1100

    Change LogMonitor to SliderMonitor

--- src/app/containers/DevTools.js
+++ src/app/containers/DevTools.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
+import SliderMonitor from 'redux-slider-monitor';
 
-export default createDevTools(<LogMonitor />);
+export default createDevTools(<SliderMonitor />);
